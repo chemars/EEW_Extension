987aa3fcee4e9d0b1be49bd6bc6c87bf7f1c6d39
chemars <37469933+chemars@users.noreply.github.com>
2018-05-05 19:00:50 +0800

    路徑修正

--- alarm.bat
+++ alarm.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 SET "filename=alarm.wav"
-PowerShell -WindowStyle Hidden -Command "(New-Object Media.SoundPlayer "%~dp0\%filename%").PlaySync();"
+PowerShell -WindowStyle Hidden -Command "(New-Object Media.SoundPlayer "%~dp0%filename%").PlaySync();"
